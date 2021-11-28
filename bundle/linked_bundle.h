7ec05c01bf07b4945cda98d9b71eb8f3c9ba80d6
Mickias Bekele <mickiasb@gmail.com>
2021-11-28 03:09:50 -0500

    finished linked_bundle.h

--- bundle/linked_bundle.h
+++ bundle/linked_bundle.h
@@ -32,37 +32,11 @@ template <typename NodeType>
 
 class BundleEntry : public std::vector<std::shared_ptr<BundleEntryBase<NodeType>>> {
  public:
-  // volatile timestamp_t ts_;  // Redefinition of ts_ to make it volitile.
-
-  // Additional members.
-  //BundleEntry *volatile next_;
-
   std::weak_ptr<BundleEntryBase<NodeType>> get(std::size_t i){
     auto ret = std::weak_ptr<BundleEntryBase<NodeType>>{};
     ret = std::vector<std::shared_ptr<BundleEntryBase<NodeType>>>::operator[]{i};
     return ret;
   }
-  // volatile timestamp_t deleted_ts_;
-
-  // explicit BundleEntry(timestamp_t ts, NodeType *ptr, BundleEntry *next, size_t i)
-  //     : ts_(ts), next_(next) {
-    
-  //   this->ptr_ = ptr;
-  //   deleted_ts_ = BUNDLE_NULL_TIMESTAMP;
-  // }
-
-  // void set_ts(const timestamp_t ts) { ts_ = ts; }
-  // void set_ptr(NodeType *const ptr) { this->ptr_ = ptr; }
-  // void set_next(BundleEntry *const next) { next_ = next; }
-
-  // timestamp_t marked() { return deleted_ts_; }
-
-  // inline void validate() {
-  //   if (ts_ < next_->ts_) {
-  //     std::cout << "Invalid bundle" << std::endl;
-  //     exit(1);
-  //   }
-  // }
 };
 
 template <typename NodeType>
@@ -79,40 +53,23 @@ class Bundle : public BundleInterface<NodeType> {
  public:
   ~Bundle() {
     head_.clear();
-    // BundleEntry<NodeType> *curr = head_;
-    // BundleEntry<NodeType> *next;
-    // while (curr != tail_) {
-    //   next = curr->next_;
-    //   delete curr;
-    //   curr = next;
-    // }
-    // delete tail_;
   }
 
   void init() override {
     head_.emplace_back(std::make_shared<BundleEntryBase<NodeType>>(BUNDLE_NULL_TIMESTAMP, nullptr));
-    // tail_ = new BundleEntry<NodeType>(BUNDLE_NULL_TIMESTAMP, nullptr, nullptr);
-    //head_ = tail_;
   }
 
   // Inserts a new rq_bundle_node at the head of the bundle.
   inline void prepare(NodeType *const ptr) override {
-    BundleEntryBase<NodeType> *new_entry = 
-        new BundleEntryBase<NodeType>(BUNDLE_PENDING_TIMESTAMP, ptr);
     BundleEntry<NodeType> new_head;
-    new_head.emplace_back(new_entry);
-    //head_.emplace_back(std::make_shared<BundleEntry<NodeType>>(BUNDLE_PENDING_TIMESTAMP, ptr));
-    // BundleEntry<NodeType> *new_entry =
-    //     new BundleEntry<NodeType>(BUNDLE_PENDING_TIMESTAMP, ptr, nullptr);
+    new_head.emplace_back(std::make_shared<BundleEntryBase<NodeType>>(BUNDLE_PENDING_TIMESTAMP, ptr));
     
     auto expected;
     while (true) {
       expected = head_;
       // expected = head_;
       new_head.emplace_back(expected.get(0));
-      // new_entry->next_ = expected;
       new_head[0]->neighbors.emplace_back(new_head.get(1));
-      new_head[1]->neighbors.emplace_back(new_head.get(0));
       long i = 0;
       while (expected[0]->ts_ == BUNDLE_PENDING_TIMESTAMP) {
         // DEBUG_PRINT("insertAtHead");
@@ -140,14 +97,13 @@ class Bundle : public BundleInterface<NodeType> {
     auto size = head_.size();
     auto curr = head_.get(0);
     long i = 0;
-    // bool *visited = new bool[size]; 
     while (curr.lock()->ts_ == BUNDLE_PENDING_TIMESTAMP) {
       // DEBUG_PRINT("getPtrByTimestamp");
       CPU_RELAX;
     }
    
     curr.lock()->visited_ = true;
-    list<BundleEntryBase<NodeType> *> queue;
+    std::list<std::shared_ptr<BundleEntryBase<NodeType>>> queue;
     queue.push_back(curr);
     // 'i' will be used to get all adjacent
     // vertices of a vertex
@@ -164,15 +120,11 @@ class Bundle : public BundleInterface<NodeType> {
             }
         }
     }
-    for each (auto& u in head_){
+    for(auto& u : head_){
       if(u){
-        u.lock()->visited_ = false
+        u.lock()->visited_ = false;
       }
     }
-    // while (curr != tail_ && curr->ts_ > ts) {
-    //   assert(curr->ts_ != BUNDLE_NULL_TIMESTAMP);
-    //   curr = curr->next_;
-    // }
 #ifdef BUNDLE_DEBUG
     if (curr->marked()) {
       std::cout << dump(0) << std::flush;
@@ -187,125 +139,112 @@ class Bundle : public BundleInterface<NodeType> {
   inline void reclaimEntries(timestamp_t ts) override {
     // Obtain a reference to the pred non-reclaimable entry and first
     // reclaimable one.
-    BundleEntry<NodeType> *pred = head_.get(0);
+    auto pred = head_.get(0);
     long i = 0;
-    list<BundleEntryBase<NodeType> *> Q;
+    std::list<std::shared_ptr<BundleEntryBase<NodeType>>> Q;
     Q.push_back(pred);
     while (pred.lock()->ts_ == BUNDLE_PENDING_TIMESTAMP && !Q.empty()) {
       pred = Q.front();
       Q.pop_front();
       
       auto x;
-      for(x = pred.lock()->neighbors.begin(); x != pred.lock()->neighbors.end(); x++){
+      for(x = pred.lock()->neighbors.begin(); x != pred.lock()->neighbors.end(); ++x){
         if(!x.lock()->visited_){
           x.lock()->visited_ = true;
           Q.push_back(x);
         }
       }
-      // DEBUG_PRINT("reclaimEntries");
-      // pred = pred->next_;
     }
     Q.clear();
     SOFTWARE_BARRIER;
-    // BundleEntry<NodeType> *curr = pred->next_;
-    std::vector<std::weak_ptr<BundleEntry<NodeType>>> currNodes = pred.lock()->neighbors;
-    BundleEntry<NodeType> *curr;
+    auto currNodes = pred.lock()->neighbors;
+    
     if (pred.lock()->ts = BUNDLE_NULL_TIMESTAMP|| currNodes.empty()) {
       return;  // Nothing to do.
     }
 
-    curr = currNodes[0];
+    auto curr = currNodes[0];
     // If there are no active RQs then we can recycle all edges, but the
     // newest (i.e., head). Similarly if the oldest active RQ is newer than
     // the newest entry, we can reclaim all older entries.
     if (ts == BUNDLE_NULL_TIMESTAMP || pred.lock()->ts_ <= ts) {
-      // pred->next_ = tail_;
       pred.lock()->neighbors = {};
     } else {
       // Traverse from head and remove nodes that are lower than ts.
       Q.push_back(curr);
-      while(curr.lock()->ptr_ != nullptr && curr.lock()->ts > ts){
+      while(curr.lock()->ptr_ != nullptr && curr.lock()->ts_ > ts){
         curr = Q.front();
-        pred = curr;
         Q.pop_front();
 
         auto x;
-        for(x)
+        for(x = curr.lock()->neighbors.begin(); x != curr.lock()->neighbors.end(); ++x){
+            if(!x.lock()->visited_){
+              x.lock()->visited_ = true;
+              Q.push_back(x);
+            }
+        }
       }
-      while (curr != tail_ && curr->ts_ > ts) {
-        
+      Q.clear();
+
+      if(curr.lock()->ptr_ != nullptr){
         pred = curr;
-        curr = curr->next_;
+        currNodes = curr.lock()->neighbors;
+        pred.lock()->neighbors = {};
       }
-      if (curr != tail_) {
-        // Curr points to the entry required by the oldest timestamp. This entry
-        // will become the last entry in the bundle.
-        pred = curr;
-        curr = curr->next_;
-        pred->next_ = tail_;
-      }
+
     }
 #ifdef BUNDLE_DEBUG
     last_recycled = curr;
-    oldest_edge = pred->ts_;
+    oldest_edge = pred.lock()->ts_;
 #endif
 
     // Reclaim nodes.
-    assert(curr != head_ && pred->next_ == tail_);
-    while (curr != tail_) {
+    Q.push_back(curr);
+    while(!Q.empty()){
+      curr = Q.front();
+      Q.pop_front();
+      if(curr.lock()->ptr_ == nullptr){
+        continue;
+      } 
+      auto x;
+      for(x = curr.lock()->neighbors.begin(); x != curr.lock()->neighbors.end(); ++x){
+        if(!x.lock()->visited_){
+          x.lock()->visited_ = true;
+          Q.push_back(x);
+        }
+      }
       pred = curr;
-      curr = curr->next_;
-      pred->mark(ts);
+      pred.lock()->mark(ts);
+
 #ifndef BUNDLE_CLEANUP_NO_FREE
       delete pred;
 #endif
     }
-#ifdef BUNDLE_DEBUG
-    if (curr != tail_) {
-      std::cout << curr << std::endl;
-      std::cout << dump(ts) << std::flush;
-      exit(1);
+
+    //return visited back to false
+    for(auto& u : head_){
+      if(u){
+        u.lock()->visited_ = false;
+      }
     }
-#endif
   }
 
   // [UNSAFE] Returns the number of bundle entries.
   int size() override {
-    int size = 0;
-    BundleEntry<NodeType> *curr = head_;
-    while (curr != tail_) {
-#ifdef BUNDLE_DEBUG
-      if (curr->marked()) {
-        std::cout << dump(0) << std::flush;
-        exit(1);
-      }
-#endif
-      ++size;
-      curr = curr->next_;
-#ifdef BUNDLE_DEBUG
-      if (curr == nullptr) {
-        std::cout << dump(0) << std::flush;
-        exit(1);
-      }
-#endif
-    }
+    int size = head_.size() - 1;
     return size;
   }
 
   inline NodeType *first(timestamp_t &ts) override {
-    BundleEntry<NodeType> *entry = head_;
-    ts = entry->ts_;
-    return entry->ptr_;
+    auto entry = head_.get(0);
+    ts = entry.lock()->ts_;
+    return entry.lock()->ptr_;
   }
 
   std::pair<NodeType *, timestamp_t> *get(int &length) override {
     // Find the number of entries in the list.
-    BundleEntry<NodeType> *curr_entry = head_;
-    int size = 0;
-    while (curr_entry != tail_) {
-      ++size;
-      curr_entry = curr_entry->next_;
-    }
+    auto curr_entry = head_.get(0);
+    int size = head_.size() - 1;
 
     // Build the return array.
     std::pair<NodeType *, timestamp_t> *retarr =
@@ -313,30 +252,63 @@ class Bundle : public BundleInterface<NodeType> {
     int pos = 0;
     NodeType *ptr;
     timestamp_t ts;
-    curr_entry = head_;
-    while (curr_entry != tail_) {
-      ptr = curr_entry->ptr_;
-      ts = curr_entry->ts_;
+    std::list<std::shared_ptr<BundleEntryBase<NodeType>>> queue;
+    queue.push_back(curr_entry);
+
+    //Traverse through each node
+    while(!queue.empty()){
+      curr_entry = queue.front();
+      queue.pop_front();
+      ptr = curr_entry.lock()->ptr_;
+      ts = curr_entry.lock()->ts_;
       retarr[pos++] = std::pair<NodeType *, timestamp_t>(ptr, ts);
-      curr_entry = curr_entry->next_;
+
+      auto x;
+      for(x = curr_entry.lock()->neighbors.begin(); x != curr_entry.lock()->neighbors.end(); ++x){
+        if(!x.lock()->visited_){
+          x.lock()->visited_ = true;
+          queue.push_back(x);
+        }
+      }
+    }
+
+    //reverse back visited to false again
+    for(auto& u : head_){
+      if(u){
+        u.lock()->visited_ = false;
+      }
     }
     length = size;
     return retarr;
   }
 
   string __attribute__((noinline)) dump(timestamp_t ts) {
-    BundleEntry<NodeType> *curr = head_;
+    auto curr = head_.get(0);
     std::stringstream ss;
+
     ss << "(ts=" << ts << ") : ";
     long i = 0;
-    while (curr != nullptr && curr != tail_) {
-      ss << "<" << curr->ts_ << "," << curr->ptr_ << "," << curr->next_ << ">"
+
+    std::list<std::shared_ptr<BundleEntryBase<NodeType>>> queue;
+    queue.push_back(curr);
+
+    //Traverse through each node
+    while(!queue.empty()){
+      curr = queue.front();
+      queue.pop_front();
+      ss << "<" << curr.lock()->ts_ << "," << curr.lock()->ptr_ << ">"
          << "-->";
-      curr = curr->next_;
+      auto x;
+      for(x = curr.lock()->neighbors.begin(); x != curr.lock()->neighbors.end(); ++x){
+        if(!x.lock()->visited_){
+          x.lock()->visited_ = true;
+          queue.push_back(x);
+        }
+      }
     }
-    if (curr == tail_) {
-      ss << "(tail)<" << curr->ts_ << "," << curr->ptr_ << ","
-         << (long)curr->next_ << ">";
+
+    if (curr.lock()->ptr == nullptr) {
+      ss << "(tail)<" << curr.lock()->ts_ << "," << curr->ptr_ << ">";
     } else {
       ss << "(unexpected end)";
     }
